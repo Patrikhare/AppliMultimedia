85c09ab8f4facd30545338bf6d09afdecf72b747
GreenThorns <36452295+GreenThorns@users.noreply.github.com>
2019-05-09 11:18:51 +0200

    Add Erosion + Clean Project

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,3 +1,5 @@
+#include "pch.h"
+
 #include <opencv2/opencv.hpp>
 
 #include <iostream>
@@ -6,32 +8,38 @@
 using namespace cv;
 using namespace std;
 
-Mat reference = imread("van_gogh.jpg");
+Mat reference = imread("E:\\van_gogh.jpg");
 Mat image = reference.clone();
 Mat temp = image.clone();
 String selectedTool = "None";
-Mat brightness_dst;
 
+//Brightness tool
+Mat brightness_dst;
 int beta = 50;
 int alpha = 50;
 int const max_beta = 100;
 int const max_alpha = 100;
 
+//ErosionTool
+Mat ErosionDilation_dst;
+int erosiondilation = 0;
+int erosiondilation_size = 0;
+int erosiondilation_elem = 0;
+int const max_elem = 2;
+int const max_size = 21;
 
 //Variable for Canny edge detection tool
 
 Mat src_gray;
 Mat dst, detected_edges;
-Mat src = reference.clone();
 
 int edgeThresh = 1;
 int lowThreshold;
 int const max_lowThreshold = 100;
-int ratio = 3;
+int edge_ratio = 3;
 int kernel_size = 3;
 
-char* window_name = "Edge Map";
-
+String window_name = "Edge Map";
 
 void mouse_callback(int event, int x, int y, int flags, void* param) {
 
@@ -52,6 +60,18 @@ void mouse_callback(int event, int x, int y, int flags, void* param) {
 		//cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
 
 	}
+
+}
+
+void help() {
+
+	cout << "Help:" << endl;
+	cout << "ESC: Exit" << endl;
+	cout << "B: Brightness Tool" << endl;
+	cout << "C: Clear modifications" << endl;
+	cout << "E: Erosion Tool" << endl;
+	cout << "F: Canny Edge Detection Tool" << endl;
+	cout << "R: Resize Tool" << endl;
 
 }
 
@@ -99,10 +119,9 @@ void brightness_tool() {
 		//2621440 = Bas
 		//2555904 = Droite
 
-		//k = waitKeyEx(20); //OpenCv V3
-		k = waitKey(20); //OpenCv V2
+		k = waitKeyEx(20);
 
-		if (k == 27 || k ==1048603) { //ESC
+		if (k == 27 || k == 1048603) { //ESC
 			break;
 		}
 
@@ -117,60 +136,127 @@ void brightness_tool() {
 
 	cout << "Back to Menu" << endl;
 	destroyWindow(windowName);
+	help();
 	
 }
 
+void ErosionDilation(int, void*)
+{
+	int erosiondilation_type = 0;
+	if (erosiondilation_elem == 0) { erosiondilation_type = MORPH_ELLIPSE; }
+	else if (erosiondilation_elem == 1) { erosiondilation_type = MORPH_RECT; }
+	else if (erosiondilation_elem == 2) { erosiondilation_type = MORPH_CROSS; }
+
+	if (erosiondilation == 0) {
+		Mat element = getStructuringElement(erosiondilation_type,
+			Size(2 * erosiondilation_size + 1, 2 * erosiondilation_size + 1),
+			Point(erosiondilation_size, erosiondilation_size));
+		erode(image, ErosionDilation_dst, element);
+	}
+	else if (erosiondilation == 1) {
+		Mat element2 = getStructuringElement(erosiondilation_type,
+			Size(2 * erosiondilation_size + 1, 2 * erosiondilation_size + 1),
+			Point(erosiondilation_size, erosiondilation_size));
+		dilate(image, ErosionDilation_dst, element2);
+
+	}
+	imshow("Erosion/Dilation", ErosionDilation_dst);
+}
+
+void erosion_tool() {
+
+	String windowName = "Erosion/Dilation";
+
+	namedWindow(windowName, WINDOW_AUTOSIZE);
+	moveWindow(windowName, image.cols, 0);
+	createTrackbar("erosion or dilation:\n 0: dilation \n 1: erosion ", windowName,
+		&erosiondilation, 1,
+		ErosionDilation);
+	createTrackbar("shape type:\n 0: Rect \n 1: Cross \n 2: Ellipse", windowName,
+		&erosiondilation_elem, max_elem,
+		ErosionDilation);
+	createTrackbar("size:\n 2n +1", windowName,
+		&erosiondilation_size, max_size,
+		ErosionDilation);
+	ErosionDilation(0, 0);
+
+	int k = 0;
+	while (k != 27) {
+
+		//2490368 = Haut
+		//2424832 = Gauche
+		//2621440 = Bas
+		//2555904 = Droite
+
+		k = waitKeyEx(20);
+
+		if (k == 27 || k == 1048603) { //ESC
+			break;
+		}
+
+		if (k == 115 || k == 1048691) { //S
+			ErosionDilation_dst.copyTo(image);
+			break;
+		}
+
+
+
+	}
+
+	cout << "Back to Menu" << endl;
+	destroyWindow(windowName);
+	help();
+}
 
 void CannyThreshold(int, void*)
 {
-  /// Reduce noise with a kernel 3x3
-  blur( src_gray, detected_edges, Size(3,3) );
+	/// Reduce noise with a kernel 3x3
+	blur(src_gray, detected_edges, Size(3, 3));
 
-  /// Canny detector
-  Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+	/// Canny detector
+	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*edge_ratio, kernel_size);
 
-  /// Using Canny's output as a mask, we display our result
-  dst = Scalar::all(0);
+	/// Using Canny's output as a mask, we display our result
+	dst = Scalar::all(0);
 
-  src.copyTo( dst, detected_edges);
-  imshow( window_name, dst );
- }
+	image.copyTo(dst, detected_edges);
+	imshow(window_name, dst);
+}
 
-void edgeDetection(){
+void edgeDetection() {
 
-  int sKey = 1048691;
-  int escapeKey = 1048603;
-  int k = 0;
-  
-  while (k != escapeKey) { //while the escape button is not hit 
+	int sKey = 1048691;
+	int escapeKey = 1048603;
+	int k = 0;
 
-    if (k == sKey || k == 114){
+	while (k != 27) { //while the escape button is not hit 
 
-      cout << "Image saved !"<< endl;
-      imwrite("cannyImage.jpg", dst);
-      break;
-    }
+		dst.create(image.size(), image.type());
 
-    dst.create( src.size(), src.type() );
+		/// Convert the image to grayscale
+		cvtColor(image, src_gray, COLOR_BGR2GRAY);
 
-    /// Convert the image to grayscale
-    cvtColor( src, src_gray, CV_BGR2GRAY );
+		/// Create a window
+		namedWindow(window_name, WINDOW_AUTOSIZE);
 
-    /// Create a window
-    namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+		/// Create a Trackbar for user to enter threshold
+		createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
 
-    /// Create a Trackbar for user to enter threshold
-    createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+		/// Show the image
+		CannyThreshold(0, 0);
 
-    /// Show the image
-    CannyThreshold(0, 0);
+		//k = waitKeyEx(0); //V3
+		k = waitKey(0); //V2
 
-    //k = waitKeyEx(0); //V3
-    k = waitKey(0); //V2
+		if (k == 27 || k == escapeKey) { //ESC
+			break;
+		}
 
+	}
 
-
-  }
+	cout << "Back to Menu" << endl;
+	destroyWindow(window_name);
+	help();
 
 }
 
@@ -188,8 +274,7 @@ void resize_tool() {
 		imshow(windowName, aff);
 
 
-		//k = waitKeyEx(0); //V3
-    	k = waitKey(0); //V2
+		k = waitKeyEx(0);
 		
 		if (k == 2490368) {
 			addY += 0.1;
@@ -222,6 +307,7 @@ void resize_tool() {
 
 	cout << "Back to Menu" << endl;
 	//destroyWindow(windowName);
+	help();
 }
 
 int cropAndResize_students() {
@@ -274,6 +360,8 @@ void clear() {
 int main(int argc, char** argv)
 {
 
+	help();
+
 	if (reference.empty())
 	{
 		cout << "Could not open or find the image" << endl;
@@ -288,32 +376,34 @@ int main(int argc, char** argv)
 
 	int k = 0;
 
-	while (k != 27 || k != 1048603) {
+	while (k != 27) {
 
 		imshow(windowName, image);
 		//putText(temp, "ESC to exit / c to clear", Point(10, 30), FONT_HERSHEY_PLAIN, 1, Scalar(255, 255, 255), 2);
-		
-		//k = waitKeyEx(0); //V3
-    	k = waitKey(0); //V2
-
+		k = waitKeyEx(20);
 		//cout << k << endl;
-		if (k == 99 || k== 1048675) { //C
+		if (k == 99 || k == 1048675) { //C
 			cout << "Image cleared" << endl;
 			clear();
 		}
 
-		if (k == 98 || k== 1048674) { //B
+		if (k == 98 || k == 1048674) { //B
 			cout << "Tool: Brightness selected" << endl;
 			brightness_tool();
 		}
 
-		if (k == 114 || k== 1048690 ) { //R
+		if (k == 114 || k == 1048690) { //R
 			cout << "Tool: Resize selected" << endl;
 			resize_tool();
 		}
 
-		if((k == 101 || k == 1048677)){ //s
-			cout << "Tool: Canny edge detection selected" << endl;
+		if (k == 101 || k == 1048677) { //E
+			cout << "Tool: Erosion selected" << endl;
+			erosion_tool();
+		}
+
+		if (k == 102) { //F à rajouter touche OpenCV 2.
+			cout << "Tool: Edge Detection selected" << endl;
 			edgeDetection();
 		}
 	}
